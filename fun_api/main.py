ece7705381be12508045945493dcec7fd3de6396
RmStorm <roald.storm@oda.com>
2022-02-22 10:50:49 +0100

    Added a bunch of stuff to see how things behave when pods die under load

--- fun_api/main.py
+++ fun_api/main.py
@@ -1,16 +1,46 @@
+import signal
+import time
+import asyncio
 import logging
 import os
 from distutils.util import strtobool
 
 from typing import Optional
 
+import httpx
 from fastapi import FastAPI, HTTPException, Request
 from prometheus_client import Counter, make_asgi_app
+from prometheus_fastapi_instrumentator import Instrumentator, metrics
 
 echo_counter = Counter('my_echo_counter', 'Description of counter')
+http_load_counter = Counter('roald_request_counter', 'attempts to request the cool api', labelnames=["status"])
 
 app = FastAPI()
+Instrumentator().instrument(app)
+
+instrumentator = Instrumentator()
+instrumentator.add(metrics.default(metric_namespace='roald'))
+instrumentator.instrument(app)
 app.mount("/metrics", make_asgi_app())
+
+
+def _loglevel_signal_handler():
+    fastapi_sigterm_handler = signal.getsignal(signal.SIGTERM)
+
+    def handle_sigterm(signalnum, _frame):
+        if not signalnum == signal.SIGTERM:
+            raise RuntimeError('WTF is going on')
+        print('Received SIGTERM now!!!!')
+        fastapi_sigterm_handler(signalnum, _frame)
+
+    signal.signal(signal.SIGTERM, handle_sigterm)
+
+
+@app.on_event("startup")
+async def startup_event():
+    _loglevel_signal_handler()
+    print(os.getpid())
+    print(signal.getsignal(signal.SIGTERM))
 
 
 class ConfigurableChecks:
@@ -28,6 +58,44 @@ class ConfigurableChecks:
 
 
 CONFIGURABLE_CHECKS = ConfigurableChecks()
+
+
+async def deferred_get(address: str, i: int):
+    logging.info(f"getting {i}")
+    try:
+        async with httpx.AsyncClient() as client:
+            r = await client.get(address)
+            http_load_counter.labels(r.status_code).inc()
+    except httpx.TimeoutException:
+        http_load_counter.labels("504").inc()
+    except httpx.ConnectError:
+        http_load_counter.labels("502").inc()
+    logging.info(f"completed {i}")
+
+
+@app.post("/start_load")
+async def start_load(seconds: float, address: str):
+    start_time = time.time()
+    logging.info(f"Starting load at: {start_time}")
+    logging.info(f"hitting up: {address}")
+    i = 0
+    while time.time() < start_time+seconds:
+        i += 1
+        await asyncio.sleep(0.01)
+        logging.info("sending")
+        asyncio.create_task(deferred_get(address, i))
+    logging.info(f"completed load at: {start_time}")
+    return "Load completed"
+
+
+@app.get("/proxy")
+async def proxy(address: str):
+    print("sleeping")
+    await asyncio.sleep(1)
+    async with httpx.AsyncClient() as client:
+        r = await client.get(address)
+        r.raise_for_status()
+    return {'state': 'success', 'proxied_response': r.json()}
 
 
 @app.get("/")
